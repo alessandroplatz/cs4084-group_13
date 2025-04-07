29f1d87baebd0c3afca31ef768f8f9d104ecbdb9
Eric1641 <ericlonerganat@gmail.com>
2025-04-07 21:06:16 +0100

    Adde basic Sql
    It has not been tested but im gonna push and change if needs be.

--- app/src/main/java/com/example/cs4084_group_13/DBHandler.java
+++ app/src/main/java/com/example/cs4084_group_13/DBHandler.java
@@ -1,5 +1,43 @@
 package com.example.cs4084_group_13;
 
-public class DBHandler {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DBHandler extends SQLiteOpenHelper {
+
+    private static final String DB_Name =   "CollectionsDB";
+    private static final int DB_VERSION= 1;
+    private static final String ID_COL = "Collection_ID";
+    private static final String COL_1 = "Name";
+    private static final String TABLE_NAME = "Collections";
+
+    public DBHandler (Context context) {
+    super(context,DB_Name,null,DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "Create table " + TABLE_NAME + " ("
+        +ID_COL + "Integer Primary Key AUTOINCREMENT, "
+        + COL_1 + " Text";
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        //just here to make no error
+    }
+
+    public void add(String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values =new ContentValues();
+
+        values.put(COL_1, name);
+        db.insert(TABLE_NAME,null,values);
+    }
+    
 
 }
