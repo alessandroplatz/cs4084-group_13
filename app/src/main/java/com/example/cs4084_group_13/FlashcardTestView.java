750c3967cabfc5ad3930e6c3e57f41a2142e11ef
Eric1641 <ericlonerganat@gmail.com>
2025-04-29 05:42:10 +0100

    Added basic test state machine using button listeners
    the logic for a test is almost set up i just need to tie up a few loose ends such as results screen and maybe storing answers to look back on as well as maybe storing wrong answers.

--- app/src/main/java/com/example/cs4084_group_13/FlashcardTestView.java
+++ app/src/main/java/com/example/cs4084_group_13/FlashcardTestView.java
@@ -1,7 +1,11 @@
 package com.example.cs4084_group_13;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,7 +16,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class FlashcardTestView extends AppCompatActivity {
+
+    private int score = 0;
+    private int streak = 0;
+    private int question = 1;
+    private int bestStreak = 0;
+    private Flashcard currentFC;
+    private boolean finishTest = false;
+    private String FCBack;
+    private String FCFront;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +40,7 @@ public class FlashcardTestView extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
 
         TextView countText = findViewById(R.id.TextTime);
         TextView titletime = findViewById(R.id.TitleTime);
@@ -44,11 +61,192 @@ public class FlashcardTestView extends AppCompatActivity {
             public void onFinish() {
                 countText.setVisibility(TextView.GONE);
                 titletime.setVisibility(TextView.GONE);
+                testStart();
             }
         }.start();
     }
 
     public void testStart() {
+        Intent intent = getIntent();
+        int colid = intent.getIntExtra("collection_id",0);
+        DBHandler db = new DBHandler(FlashcardTestView.this);
+        ArrayList<Flashcard> flashcards = db.getFlashcardsForTest(colid);
+
+
+
+        currentFC = flashcards.remove(0);
+         FCFront = currentFC.getFront();
+         FCBack = currentFC.getBack();
+        int FCid = currentFC.getid();
+
+        LinearLayout answerbox = findViewById(R.id.flashcardLL);
+        LinearLayout confirmBox = findViewById(R.id.resultBoxLL);
+        LinearLayout failureBox = findViewById(R.id.FailureLL);
+        LinearLayout successBox = findViewById(R.id.SuccessLL);
+        TextView front = findViewById(R.id.frontTV);
+        TextView back = findViewById(R.id.backTV);
+        TextView currentStreak = findViewById(R.id.YCSTV);
+        TextView longestStreak = findViewById(R.id.LSTV);
+        TextView questionCount = findViewById(R.id.QuestionCountTV);
+        Button subBut = findViewById(R.id.subBut);
+        Button NTTbut = findViewById(R.id.NTTbut);
+        Button SOBut = findViewById(R.id.SObut);
+        Button NQBut = findViewById(R.id.NQbut);
+
+        front.setText(FCFront);
+
+
+        subBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                back.setText(FCBack);
+                if (!flashcards.isEmpty()) {
+                    currentFC = flashcards.remove(0);
+                } else {
+                    finishTest = true;
+                }
+
+                answerbox.animate()
+                                .alpha(0f)
+                                        .setDuration(500)
+                                                .withEndAction(new Runnable() {
+                                                    @Override
+                                                    public void run() {
+                                                        answerbox.setVisibility(View.GONE);
+
+
+                                                        confirmBox.setAlpha(0f);
+                                                        confirmBox.setVisibility(View.VISIBLE);
+                                                        confirmBox.animate()
+                                                                .alpha(1f)
+                                                                .setDuration(500)
+                                                                .start();
+                                                    }
+                                                }).start();
+
+
+
+
+
+            }
+        });
+
+        NTTbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bestStreak < streak) {
+                    bestStreak = streak;
+                }
+                streak = 0;
+                if (finishTest) {
+                    confirmBox.animate()
+                            .alpha(0f)
+                            .setDuration(500)
+                            .withEndAction(new Runnable() {
+                                @Override
+                                public void run() {
+                                    confirmBox.setVisibility(View.GONE);
+
+                                    confirmBox.setAlpha(0f);
+                                }
+                            }).start();
+                } else {
+                    longestStreak.setText("Longest Streak\n" + bestStreak);
+                    confirmBox.animate()
+                            .alpha(0f)
+                            .setDuration(500)
+                            .withEndAction(new Runnable() {
+                                @Override
+                                public void run() {
+                                    confirmBox.setVisibility(View.GONE);
+
+                                    confirmBox.setAlpha(0f);
+                                    failureBox.setVisibility(View.VISIBLE);
+                                    failureBox.animate()
+                                            .alpha(1f)
+                                            .setDuration(500)
+                                            .start();
+                                }
+                            }).start();
+                    //set streak text
+                }
+            }
+        });
+
+        SOBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                streak++;
+                score++;
+                if (finishTest) {
+                    confirmBox.animate()
+                            .alpha(0f)
+                            .setDuration(500)
+                            .withEndAction(new Runnable() {
+                                @Override
+                                public void run() {
+                                    confirmBox.setVisibility(View.GONE);
+
+                                    confirmBox.setAlpha(0f);
+                                }
+                            }).start();
+                } else {
+                    currentStreak.setText("Your Current Streak\n " + streak);
+
+                    confirmBox.animate()
+                            .alpha(0f)
+                            .setDuration(500)
+                            .withEndAction(new Runnable() {
+                                @Override
+                                public void run() {
+                                    confirmBox.setVisibility(View.GONE);
+
+                                    confirmBox.setAlpha(0f);
+                                    successBox.setVisibility(View.VISIBLE);
+                                    successBox.animate()
+                                            .alpha(1f)
+                                            .setDuration(500)
+                                            .start();
+                                }
+                            }).start();
+                }
+            }
+        });
+
+        NQBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FCBack =currentFC.getBack();
+                FCFront = currentFC.getFront();
+                back.setText(FCBack);
+                front.setText(FCFront);
+                question++;
+                questionCount.setText("Question. "+question);
+
+                successBox.animate()
+                        .alpha(0f)
+                        .setDuration(500)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                successBox.setVisibility(View.GONE);
+
+                                successBox.setAlpha(0f);
+                                answerbox.setVisibility(View.VISIBLE);
+                                answerbox.animate()
+                                        .alpha(1f)
+                                        .setDuration(500)
+                                        .start();
+                            }
+                        }).start();
+            }
+        });
+
+
+
+
+        answerbox.setVisibility(TextView.VISIBLE);
+
 
     }
 }
